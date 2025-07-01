c8fbfe55af54945cc3ee743984484127ba87f9a4
MateoMendietauwu69 <mendietamateo510@gmail.com>
2025-06-30 21:39:24 -0300

    menu agregado

--- EJERCICIO2-TP5.cpp
+++ EJERCICIO2-TP5.cpp
@@ -1,57 +1,142 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-typedef struct tnodo *pnodo;
-typedef struct tnodo{
+typedef struct tnodo* pnodo;
+
+typedef struct tnodo {
 	int dato;
 	pnodo siguiente;
 };
-typedef struct tlista{
+
+typedef struct tlista {
 	pnodo inicio;
 	pnodo final;
+	int contador; 
 };
 
-int main () {
+void iniciar_lista(tlista &lista);
+void crearnodo(pnodo &nuevo);
+void agregar_final(tlista &lista, pnodo nuevo);
+void mostrar(tlista lista);
+int cantidad_elementos(tlista lista); 
+pnodo eliminar_inicio(tlista &lista);
+
+int main() {
+	tlista lista;
+	pnodo nuevo, eliminado;
+	iniciar_lista(lista);
 	
+	int opcion;
+	do {
+		cout << "--- MENU ---"<<endl;
+		cout << "1. Agregar nodo al final"<<endl;
+		cout << "2. Eliminar nodo del inicio"<<endl;
+		cout << "3. Mostrar lista"<<endl ;
+		cout << "4. Cantidad de elementos"<<endl ;
+		cout << "0. Salir" << endl;
+		cout << "Opción: " << endl;
+		cin >> opcion;
+		
+		switch (opcion) {
+		case 1:
+			crearnodo(nuevo);
+			agregar_final(lista, nuevo);
+			break;
+		case 2:
+			eliminado = eliminar_inicio(lista);
+			if (eliminado != NULL) {
+				cout << "Eliminado: " << eliminado->dato << endl;
+				delete eliminado;
+			} else {
+				cout << "Lista vacía, no se pudo eliminar." << endl;
+			}
+			break;
+		case 3:
+			mostrar(lista);
+			break;
+		case 4:
+			cout << "Cantidad de elementos: " << cantidad_elementos(lista) << endl;
+			break;
+		case 0:
+			cout << "Saliendo..." << endl;
+			break;
+		default:
+			cout << "Opción inválida." << endl;
+		}
+		
+	} while (opcion != 0);
 	
-	
+	return 0;
 }
 
-void iniciar_lista(tlista &lista){
+
+void iniciar_lista(tlista &lista) {
 	lista.inicio = NULL;
-	
 	lista.final = NULL;
+	lista.contador = 0; 
 }
-void agregar_final(tlista &lista, pnodo nuevo){
-		if(lista.inicio == NULL){
-			lista.inicio = nuevo;
-			lista.final = nuevo;
-		}
-		else{
-			lista.final->siguiente = nuevo;
-			lista.final = nuevo;
-		}
+
+void crearnodo(pnodo &nuevo) {
+	nuevo = new tnodo;
+	if (nuevo != NULL) {
+		cout << "Ingrese valor: ";
+		cin >> nuevo->dato;
+		nuevo->siguiente = NULL;
+	} else {
+		cout << "Memoria insuficiente" << endl;
 	}
-pnodo eliminar_final(tlista &lista){
-			pnodo borrado, i;
-			if(lista.inicio == NULL){
-				borrado = NULL;
-			}
-			else{
-				if(lista.inicio == lista.final){
-					borrado = lista.inicio;
-					lista.inicio = NULL;
-					lista.final = NULL;
-				}
-				else{
-					for(i = lista.inicio; (i->siguiente)->siguiente != NULL;
-					i=i->siguiente);
-					borrado = lista.final;
-					lista.final = i;
-					lista.final->siguiente = NULL;
-				}
-			}
-			return borrado;
+}
+
+void agregar_final(tlista &lista, pnodo nuevo) {
+	if (lista.inicio == NULL) {
+		lista.inicio = nuevo;
+		lista.final = nuevo;
+	} else {
+		lista.final->siguiente = nuevo;
+		lista.final = nuevo;
+	}
+	lista.contador++; 
+}
+
+pnodo eliminar_inicio(tlista &lista) {
+	pnodo borrado;
+	if (lista.inicio == NULL) {
+		borrado = NULL;
+	} else {
+		borrado = lista.inicio;
+		lista.inicio = lista.inicio->siguiente;
+		if (lista.inicio == NULL) {
+			lista.final = NULL;
 		}
-			
+		borrado->siguiente = NULL;
+		lista.contador--; 
+	}
+	return borrado;
+}
+
+void mostrar(tlista lista) {
+	pnodo aux = lista.inicio;
+	if (aux == NULL) {
+		cout << "Lista vacía" << endl;
+	} else {
+		while (aux != NULL) {
+			cout << aux->dato << " -> ";
+			aux = aux->siguiente;
+		}
+		cout << "NULL" << endl;
+	}
+}
+
+int cantidad_elementos(tlista lista) {
+	return lista.contador;
+}
+void agregar_inicio(tlista &lista, pnodo nuevo) {
+	if (lista.inicio == NULL) {
+		lista.inicio = nuevo;
+		lista.final = nuevo;
+	} else {
+		nuevo->siguiente = lista.inicio;
+		lista.inicio = nuevo;
+	}
+	lista.contador++; 
+}
